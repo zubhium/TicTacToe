49e32ec9839c50731f98b93aa5eb27a8ea45261a
KD <deshpande_koustubha@yahoo.com>
2013-01-21 12:32:20 -0800

    Integrating ZubhiumSDK to demonstrate crash reporting

--- TicTacToeMain/src/com/example/android/tictactoe/GameApplication.java
+++ TicTacToeMain/src/com/example/android/tictactoe/GameApplication.java
@@ -1,13 +1,21 @@
 package com.example.android.tictactoe;
 
+import com.zubhium.ZubhiumSDK;
+
 import android.app.Application;
 
 public class GameApplication extends Application{
-
+	private ZubhiumSDK zubhiumSDK = null;
+	private final String zubhiumAppKey = "";
+	
 	@Override
 	public void onCreate() {
-		// TODO Auto-generated method stub
 		super.onCreate();
+		// Initialize ZubhiumSDK in Global Context
+		zubhiumSDK = ZubhiumSDK.getZubhiumSDKInstance(this, zubhiumAppKey);
 	}
 
+	public ZubhiumSDK getZubhiumSDK() {
+		return zubhiumSDK;
+	}
 }
--- TicTacToeMain/src/com/example/android/tictactoe/MainActivity.java
+++ TicTacToeMain/src/com/example/android/tictactoe/MainActivity.java
@@ -26,31 +26,35 @@ import com.example.android.tictactoe.library.GameActivity;
 import com.example.android.tictactoe.library.GameView.State;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		setup();
+	}
 
-        findViewById(R.id.start_player).setOnClickListener(
-                new OnClickListener() {
-            public void onClick(View v) {
-                startGame(true);
-            }
-        });
+	private void setup() {
+		findViewById(R.id.start_player).setOnClickListener(
+				new OnClickListener() {
+					public void onClick(View v) {
+						startGame(true);
+					}
+				});
 
-        findViewById(R.id.start_comp).setOnClickListener(
-                new OnClickListener() {
-            public void onClick(View v) {
-                startGame(false);
-            }
-        });
-    }
+		findViewById(R.id.start_comp).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startGame(false);
+			}
+		});
+	}
 
-    private void startGame(boolean startWithHuman) {
-        Intent i = new Intent(this, GameActivity.class);
-        i.putExtra(GameActivity.EXTRA_START_PLAYER,
-                startWithHuman ? State.PLAYER1.getValue() : State.PLAYER2.getValue());
-        startActivity(i);
-    }
+	private void startGame(boolean startWithHuman) {
+		Intent i = new Intent(this, GameActivity.class);
+		i.putExtra(
+				GameActivity.EXTRA_START_PLAYER,
+				startWithHuman ? State.PLAYER1.getValue() : State.PLAYER2
+						.getValue());
+		startActivity(i);
+	}
 }
