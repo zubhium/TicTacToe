f3478ddd3c8b8d72175394143834fa88dc66bb0a
KD <deshpande_koustubha@yahoo.com>
2013-01-21 12:19:59 -0800

    Setting up project files for demo

---
+++ TicTacToeMain/src/com/example/android/tictactoe/GameApplication.java
@@ -0,0 +1,13 @@
+package com.example.android.tictactoe;
+
+import android.app.Application;
+
+public class GameApplication extends Application{
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+}
